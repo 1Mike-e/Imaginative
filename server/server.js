3fa6d475f2faf8acb22ee05a2f68d913bf958262
Emmanuel Kyeremeh <josephkyeremeh53@gmail.com>
2021-10-08 08:02:14 +0000

    created server

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
-dotenv.config();
+dotenv.config({ path: "./.env" });
 
 const app = express();
 
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.get("/", (req, res) => {
   res.json({ msg: "Hello World" });
